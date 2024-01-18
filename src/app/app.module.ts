bbeecf9b744ce7c5f33b11fdf71f5cb79d902996
marko.nikolic@naissus.tech <marko.nikolic@naissus.tech>
2024-01-18 12:57:39 +0100

    Remove unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TreeComponent } from './tree/tree.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,25 +12,11 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CdkTreeModule } from '@angular/cdk/tree';
 
 import { CdkTableModule } from '@angular/cdk/table';
-import { TempFakeApiService } from './tree/tempFakeApi.service';
-import { TreeNodeService } from './tree/tree.service';
-import { CdkTreeComponent } from './cdk-tree/cdk-tree.component';
-import { VirtualScrollDirective } from './virtual-scroll.directive';
-import { TestTreeComponent } from './test-tree/test-tree.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { VsTreeComponent } from './vs-tree/vs-tree.component';
 import { VsCdkTreeComponent } from './vs-cdk-tree/vs-cdk-tree.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TreeComponent,
-    CdkTreeComponent,
-    VirtualScrollDirective,
-    TestTreeComponent,
-    VsTreeComponent,
-    VsCdkTreeComponent,
-  ],
+  declarations: [AppComponent, VsCdkTreeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -44,7 +29,7 @@ import { VsCdkTreeComponent } from './vs-cdk-tree/vs-cdk-tree.component';
     CdkTableModule,
     MatProgressBarModule,
   ],
-  providers: [TempFakeApiService, TreeNodeService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
